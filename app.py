7f9bcdb0512bcf477a759805317e231e39c88a18
Jan Nitschke <jan.nitschke@pandata.de>
2020-05-07 13:05:21 +0200

    implemented decorator to set logger to debug level

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, request
 import re
 import datetime
-from utils import dev_mode
+from utils import debug
+import logging
 
 app = Flask(__name__)
-
+logger = logging.getLogger('RingRing')
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+logger.addHandler(handler)
 
 @app.route("/")
 def home():
@@ -12,14 +16,15 @@ def home():
 
 
 @app.route("/get_bot_response")
-@dev_mode
+@debug(logger=logger, _debug=False)
 def get_bot_response():
-    user_text = request.args.get('msg')
+    user_text = request.a_debugrgs.get('msg')
     state = request.args.get('state')
     if (state and state == 'alarm') or (re.search('alarm', user_text)):
         return set_alarm(user_text, state)
 
     else:
+        logger.debug("Something very secret.")
         return {'response': """I have no service registered to that request. These are the services that I can provoide: \n
         - set an alarm \n
         - order champaign \n
@@ -37,6 +42,7 @@ def set_alarm(user_text, state):
     if state == 'alarm':
         try:
             alarm_time = datetime.datetime.strptime(user_text, '%H:%M')
+            logger.info(f'Set alarm with time: {alarm_time}')
             return {'response': f"alarm time set to {alarm_time.strftime('%H:%M')}.",
                     'state': ''}
         except:
@@ -47,6 +53,6 @@ def set_alarm(user_text, state):
                 'state': 'alarm'}
 
 
-
 if __name__ == "__main__":
     app.run()
+
