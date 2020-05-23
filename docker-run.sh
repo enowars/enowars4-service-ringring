e6ddbacbcc56212a46dcd70f651dcc665c4d93ba
Florian Nachtigall <florian.nachtigall@sap.com>
2020-05-23 14:37:01 +0200

    Allow skipping build step when starting containers
    Fix docker rm -f error handling

--- docker-run.sh
+++ docker-run.sh
@@ -2,16 +2,18 @@
 
 set -e
 
-app="flask-ringring"
+app="ringring"
 db="postgres"
 
-docker rm -f $db | echo 'Container $db already stopped and removed.'
-docker rm -f $app | echo 'Container $app already stopped and removed.'
+echo -e "\e[32m\nStopping & removing containers...\e[0m"
+docker rm -f $db || echo "Container $db already stopped and removed."
+docker rm -f $app || echo "Container $app already stopped and removed."
 
-
-echo -e "\e[32m\nBuilding Dockerfiles...\e[0m"
-docker build -t $app -f App/Dockerfile .
-docker build -t $db -f Postgres/Dockerfile .
+if [[ $1 != "-s" && $1 != "--skip-build-step" ]]; then
+    echo -e "\e[32m\nBuilding Dockerfiles...\e[0m"
+    docker build -t $app -f App/Dockerfile .
+    docker build -t $db -f Postgres/Dockerfile .
+fi
 
 echo -e "\e[32m\nStarting containers...\e[0m"
 docker run -d -e POSTGRES_PASSWORD=mysecretpassword -e POSTGRES_USER=ringring -e POSTGRES_DB=service --name=$db $db
@@ -20,8 +22,8 @@ docker run -d -e PGPASSWORD=mysecretpassword -e PGHOST=postgres -p 7353:7353 --n
 echo -e "\e[32m\nSetting up container networking...\e[0m"
 docker network rm ring 2> /dev/null || true
 docker network create ring
-docker network connect ring flask-ringring
-docker network connect ring postgres
+docker network connect ring $db
+docker network connect ring $app
 
 if [[ $1 = "-f" ]]; then
     echo -e "\e[32m\nFlask app logs...\e[0m"
