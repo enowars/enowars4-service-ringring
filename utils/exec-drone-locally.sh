2d156262af82ee5a305195ec20d8ca17bb000d59
Florian Nachtigall <florian.nachtigall@sap.com>
2020-05-23 23:29:29 +0200

    Rename drone exec alias due to naming conflicts

--- utils/exec-drone-locally.sh
+++ utils/exec-drone-locally.sh
@@ -3,8 +3,13 @@
 set -e
 
 if [[ $(docker inspect -f '{{.State.Running}}' registry) != 'true' ]]; then
+    docker network rm registry-network 2> /dev/null || true
     docker network create registry-network
     docker run -d -p 5000:5000 --restart=always --net=registry-network --name registry registry:2
 fi
 
-drone exec .drone.yml --trusted --exclude trigger-vm-image-creation --include publish-docker-invoices
+if [[ -z "$1" ]]; then
+    drone exec .drone.yml --trusted --exclude trigger-vm-image-creation --pipeline $1
+else
+    drone exec .drone.yml --trusted --exclude trigger-vm-image-creation
+fi
