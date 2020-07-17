e6dc096c5cb6d5ed9be76cdb09d848afb4d54fa0
ciklista <nitschke.jan@googlemail.com>
2020-07-17 12:01:02 +0200

    bumping up keepalive conf

--- service/App/utils/invoices_connector.py
+++ service/App/utils/invoices_connector.py
@@ -9,6 +9,7 @@ logger = logging.getLogger('RingRing')
 
 
 def add_to_invoice(guest_name, service, payment_method=PAYMENT_ON_ACCOUNT, notes=None):
+    session = requests.session()
     if 'INVOICE_HOST' not in os.environ:
         logger.error(f'Could not create invoice for {service} for {guest_name}. INVOICE_HOST variabe is missing.')
         return
@@ -16,22 +17,21 @@ def add_to_invoice(guest_name, service, payment_method=PAYMENT_ON_ACCOUNT, notes
     guest_pseudonym = hashlib.md5(guest_name.encode('utf-8')).hexdigest()
     url = 'http://' + os.environ['INVOICE_HOST'] + ':7354/add'
     params = {'name': guest_pseudonym, 'item': service, 'payment-method': payment_method, 'note': notes}
-    resp = requests.post(url, params)
+    resp = session.post(url, data=params)
     return resp.json()['invoice_number']
 
 
 def get_invoices(guest_name):
+    session = requests.session()
     if 'INVOICE_HOST' not in os.environ:
         logger.error(f"Could not get invoice for '{guest_name}''. INVOICE_HOST variabe is missing.")
         return []
     if not guest_name:
         logger.warning(f"Abort getting invoice overview - mandatory parameter guest name '{guest_name}' is not set.")
         return []
-
     guest_pseudonym = hashlib.md5(guest_name.encode('utf-8')).hexdigest()
     url = 'http://' + os.environ['INVOICE_HOST'] + ':7354/'
-    response = requests.get(url, {'name': guest_pseudonym})
-
+    response = session.get(url, params={'name': guest_pseudonym})
     if response.status_code != 200:
         return []
 
@@ -39,10 +39,12 @@ def get_invoices(guest_name):
     for invoice in invoices:
         invoice['name'] = guest_name
 
+    logger.info(f"Got invoice for guest {guest_name}: {invoices}")
     return invoices
 
 
 def request_bill(guest_name):
+    session = requests.session()
     if 'INVOICE_HOST' not in os.environ:
         logger.error(f"Could not get invoice for '{guest_name}''. INVOICE_HOST variabe is missing.")
         return [], None
@@ -51,7 +53,7 @@ def request_bill(guest_name):
         return [], None
     guest_pseudonym = hashlib.md5(guest_name.encode('utf-8')).hexdigest()
     url = 'http://' + os.environ['INVOICE_HOST'] + ':7354/request-bill'
-    response = requests.get(url, {'name': guest_pseudonym})
+    response = session.get(url, params={'name': guest_pseudonym})
     if response.status_code != 200:
         logger.warning(f"Request to {url} with did not return successfully.")
         return {}
@@ -71,14 +73,15 @@ def get_invoice_by_invoice_number(invoice_number, session_id):
 
     url = 'http://' + os.environ['INVOICE_HOST'] + ':7354/invoice_details'
     params = {'invoice_number': invoice_number, 'guest_name': guest_pseudonym}
-    response = session.get(url, params = params)
+    response = session.get(url, params=params)
 
     if response.status_code != 200:
         try:
             response_text = response.text
         except:
             response_text = ""
-        logger.warning(f"Request to {url} with params {params} did not return successfully. Return code: {response.status_code}. Response_text {response_text}" )
+        logger.warning(
+            f"Request to {url} with params {params} did not return successfully. Return code: {response.status_code}. Response_text {response_text}")
         return {}
     invoice = response.json()['invoice']
 
