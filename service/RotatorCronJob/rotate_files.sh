b224c1349988a4e7723b05ebacc423f3f4d554cc
ciklista <nitschke.jan@googlemail.com>
2020-07-17 17:03:52 +0200

    added settled invoices to file rotation

--- service/RotatorCronJob/rotate_files.sh
+++ service/RotatorCronJob/rotate_files.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 mv /InvoiceApp/accounting/outstanding-invoices.log /InvoiceApp/accounting/outstanding-invoices.log.$(date +%d-%m-%Y-%H-%M-%S)
-echo "File rotated."
+mv /InvoiceApp/accounting/settled-invoices.log /InvoiceApp/accounting/settled-invoices.log.$(date +%d-%m-%Y-%H-%M-%S)
+echo "Files rotated."
