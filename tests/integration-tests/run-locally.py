46e8bb007d67443033f43607fd6dc49321da1528
Florian Nachtigall <florian.nachtigall@sap.com>
2020-07-26 11:51:17 +0200

    Make running test independent from current dir

--- tests/integration-tests/run-locally.py
+++ tests/integration-tests/run-locally.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
 
+
 if __name__ == "__main__":
     os.environ["INVOICE_SERVICE_HOST"] = "localhost"
     os.environ["RINGRING_SERVICE_HOST"] = "localhost"
@@ -8,7 +9,8 @@ if __name__ == "__main__":
     os.environ["PGHOST"] = "localhost"
     os.environ["PGPASSWORD"] = "mysecretpassword"
 
-    subprocess.run("cd ../../service/ && docker-compose -f docker-compose.test.yml up  --build -d",
-                   shell=True)
-
-    subprocess.run("pytest -v --tb=short", shell=True)
+    project_root = os.path.join(os.path.dirname(
+        os.path.abspath(__file__)), '..', '..')
+    subprocess.run(
+        f"cd {os.path.join(project_root, 'service')} && docker-compose -f docker-compose.test.yml up  --build -d", shell=True, check=True)
+    subprocess.run(f"cd {project_root} && pytest -v --tb=short", shell=True)
